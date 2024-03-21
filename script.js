db7b97437cfc44664de10034f9853dc03edeb758
ninahand <ninahand@newschool.edu>
2024-03-21 16:09:09 -0400

    firstcommit

---
+++ script.js
@@ -0,0 +1,102 @@
+let allLocations = []
+let allWeights = []
+let buttonContainer = document.querySelector(".button-container")
+let descriptionContainer = document.querySelector(".description-container")
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("./data.json")
+    .then ((response)=> response.json())
+    .then ((items)=>{
+        items.forEach((item, index)=> {
+            if(!allWeights.includes(item.weight) && item.weight){
+                allWeights.push(item.weight)
+            }
+            if(!allLocations.includes(item.location) && item.location){
+                allLocations.push(item.location)
+            }
+           let description = document.createElement("div")
+           description.innerText = item.description
+           description.setAttribute("data-weight", item.weight)
+           description.classList.add("description")
+           description.setAttribute("data-location", item.location)
+           description.classList.add("description")
+           descriptionContainer.append(description)
+        })
+
+        let allButton = document.createElement("button")
+        allButton.innerText = "All"
+        buttonContainer.append(allButton)
+
+        function showAllDescriptions() {
+            let allDescriptions = document.querySelectorAll(".description");
+            allDescriptions.forEach((description) => {
+                description.classList.remove("hidden");
+            });
+        }
+        allButton.addEventListener("click", showAllDescriptions);
+
+
+
+        allWeights.forEach((weight, index)=>{ 
+            let weightButton = document.createElement("button")
+            weightButton.innerText = weight
+            weightButton.setAttribute("data-weight", weight)
+            buttonContainer.append(weightButton)
+
+            weightButton.addEventListener("click", (e)=>{
+                console.log(e.target.dataset.weight)
+                let buttonWeight = e.target.dataset.weight
+                let allDescriptions = document.querySelectorAll(".description")
+                allDescriptions.forEach((description, index)=>{
+                    console.log(description)
+                    if (description.dataset.weight == buttonWeight){
+                        description.classList.remove("hidden")
+                    }
+                    else{
+                        description.classList.add("hidden")
+                    }
+                })
+            })
+        })
+
+        allLocations.forEach((location, index)=>{ 
+            let locationButton = document.createElement("button")
+            locationButton.innerText = location
+            locationButton.setAttribute("data-location", location)
+            buttonContainer.append(locationButton)
+
+            locationButton.addEventListener("click", (e)=>{
+                console.log(e.target.dataset.location)
+                let buttonLocation = e.target.dataset.location
+                let allDescriptions = document.querySelectorAll(".description")
+                allDescriptions.forEach((description, index)=>{
+                    console.log(description)
+                    if (description.dataset.location == buttonLocation){
+                        description.classList.remove("hidden")
+                    }
+                    else{
+                        description.classList.add("hidden")
+                    }
+                })
+            })
+        })
+
+        let noneButton = document.createElement("button")
+        noneButton.innerText = "None"
+        buttonContainer.append(noneButton)
+
+
+        function hideAllDescriptions() {
+            let allDescriptions = document.querySelectorAll(".description");
+            allDescriptions.forEach((description) => {
+            description.classList.add("hidden");
+            });
+        }
+        noneButton.addEventListener("click", hideAllDescriptions);
+
+
+    });
+    
+});
+
+//weightButton.dataset.weight = weight
